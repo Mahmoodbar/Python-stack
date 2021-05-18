79fb69332a54643228e66d747366b7afb4429fa4
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-05-18 22:53:46 +0300

    start working in django

---
+++ my__environments/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\testin codes\python_stack\my__environments\djangopy3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
