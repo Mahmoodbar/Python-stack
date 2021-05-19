9b7e4abc68017ec4f10cf7deadeb47c675c4a582
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-05-19 14:31:42 +0300

    django intro

---
+++ django/django-intro/mycounter_project/mycounter_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect, HttpResponse
+from . import views
+
+
+def index(request):
+    if 'count' not in request.session:
+        request.session['count'] = 1
+    else:
+        request.session['count'] += 1
+
+    context = {
+        'counter' : request.session['count']
+
+        }
+    return render(request,"index.html",context)
+
+def destroy(request):
+    del request.session['count']
+    return redirect("/")
+# def returncount(request):
+    
+#     return redirect('',context)
