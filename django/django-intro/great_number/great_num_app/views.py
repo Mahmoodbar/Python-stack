9336fa5e62d6807e9f8f285965d40b0e207cf1c7
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-05-20 17:25:21 +0300

    python

--- django/django-intro/great_number/great_num_app/views.py
+++ django/django-intro/great_number/great_num_app/views.py
@@ -1,15 +1,38 @@
 
+from typing import Text
 from django.shortcuts import render
-import random 	              
-random.randint(1, 100) 	
+import random 	        
+
+num = random.randint(1, 100)
 
 def first(request):
     return render(request,"index.html")
 
 def second(request):
-    number = request.POST['number']
-    if number >0 and number <100:
-        if number == 48:
-            pass
+    
+    number = int(request.POST['number'])
+    
+    if number > 0 and number < 100:
+        if number == num:
+            # num = random.randint(1, 100)
+            s = num , ' was the number!'
+            context = {
+                'color' : 'green',
+                'text': s
+            }
+            return render(request, 'index2.html', context)
+        elif number > num:
+            context = {
+                'color' : 'red',
+                'text': 'Too High'
+            }
+            return render(request, 'index2.html', context)
+        else:
+            context = {
+                'color' : 'red',
+                'text' : 'Too Low'
+            }
+            return render(request, 'index2.html', context)
+    
         
-    return render(request,'index2.html')
+    return render(request,'index.html')
