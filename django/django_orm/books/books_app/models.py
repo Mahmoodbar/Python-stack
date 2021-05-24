70598d6d5a6dac870465457b81a60137aa0aa299
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-05-24 17:54:29 +0300

    python

---
+++ django/django_orm/books/books_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Book(models.Model):
+    title= models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at= models.DateField(auto_now=True)
+    notes = models.CharField(max_length=255)
+    # authors = models.ForeignKey(Author,related_name="id1",on_delete=models.CASCADE)
+
+
+
+class Author(models.Model):
+    first_name= models.CharField(max_length=255)
+    last_name =models.CharField(max_length=255)
+    books = models.ForeignKey(Book, related_name="auts",on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at= models.DateField(auto_now=True)
+    
